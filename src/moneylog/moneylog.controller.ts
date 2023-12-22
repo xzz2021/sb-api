68353d21acd2a63762ac9bff2c0bb6adc60cd921
xzz2021 <zipeng.2016@gmail.com>
2023-12-22 16:00:25 +0800

    fulfill moneylog contidition search

--- src/moneylog/moneylog.controller.ts
+++ src/moneylog/moneylog.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { MoneylogService } from './moneylog.service';
 import { CreateMoneylogDto } from './dto/create-moneylog.dto';
 import { UpdateMoneylogDto } from './dto/update-moneylog.dto';
@@ -12,9 +12,16 @@ export class MoneylogController {
     return this.moneylogService.create(createMoneylogDto);
   }
 
+  // @Get()
+  // findAll() {
+  //   return this.moneylogService.findAll();
+  // }
+
   @Get()
-  findAll() {
-    return this.moneylogService.findAll();
+  findByCondition(@Query() joinQueryParams: {[string: string]: any}) {
+    // Query 不传值 可以拿到 所有 查询 参数
+    const { pageSize = 10, pageIndex= 1, ...searchParams } = joinQueryParams
+    return this.moneylogService.findByCondition(pageSize,pageIndex, searchParams);
   }
 
   @Get(':id')
