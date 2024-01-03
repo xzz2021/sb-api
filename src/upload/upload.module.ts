bc95f27edac7318ddfbaf06e76d0c8c243b418eb
xzz2021 <zipeng.2016@gmail.com>
2024-01-03 09:38:25 +0800

    temp add

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { join, extname } from 'path';
+// import { MulterModule } from '@nestjs/platform-express';
+// import { diskStorage } from 'multer';
+// import { join, extname } from 'path';
 
 @Module({
   imports: [
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -6,10 +6,7 @@ export class UploadService {
       ){}
 
     async uploadAvator(file){
-        // console.log('🚀 ~ file: upload.service.ts:9 ~ UploadService ~ uploadAvator ~ file:', file)
-        console.log('🚀 ~ file: upload.service.ts:11 ~ UploadService ~ uploadAvator ~ file.path:', file)
         let path = file.path.replace(/\\/g, '/') || ''
-        // console.log('🚀 ~ file: upload.service.ts:11 ~ UploadService ~ uploadAvator ~ path:', path)
         return {path}
         
     }
